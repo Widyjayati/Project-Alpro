436e95b15992ad253f7d91c50a7e0ec1b7d9ddea
serafina14 <124250107@student.upnyk.ac.id>
2026-04-11 15:39:23 +0700

    update struct pesanan dan node

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // sistem manajemen laundry
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 struct pesanan {
@@ -9,7 +10,16 @@ struct pesanan {
     float beratKg;              // 3.5
     string jenisLayanan;        // "cuci + setrika"
     float hargaPerKg;           //10000 > diisi admin
-    float totalBiaya;           //otomatis : beratKg x hargaPerKg
+    float totalHarga;           //otomatis : beratKg x hargaPerKg
     string tanggalMasuk;        //"01=04-2026" > diisi admin
     string status;              //"menunggu/proses/selesai/diambil"
+    string metodeBayar;         //"cash atau transfer dll"
 };
+
+struct Node{
+    pesanan data;
+    Node* next;
+};
+
+Node* head = NULL;
+int idCounter = 1;
